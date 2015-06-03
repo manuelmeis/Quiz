4365e25644c582b339cdfb1ebf776da12920c4d5
Manuel <manuelmeis@gmail.com>
2015-06-03 23:45:59 +0200

    Primera pregunta

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var routes=require('./routes/index');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
 
 app.use('/', routes);
 //app.use('/users', users);
