f1dfb004ee947edb21847198fc2e7535e07137fb
Manuel <manuelmeis@gmail.com>
2015-06-11 20:42:19 +0200

    Lista de preguntas
    Se agrego el paso 9 en donde ya se pueden ver la lista de preguntas
    disponibles y elegir una para contestar...

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+//router.get('/quizes/question',quizController.question);
+//router.get('/quizes/answer',quizController.answer);
+//Definición de rutas de /quezies
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', quizController.autores);
 
