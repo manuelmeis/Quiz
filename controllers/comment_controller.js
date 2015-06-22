12e28ffcd3fab433766198bfd050984505ed6e25
Manuel <manuelmeis@gmail.com>
2015-06-22 13:23:54 +0200

    Crear comentario

---
+++ controllers/comment_controller.js
@@ -0,0 +1,26 @@
+var models=require('../models/models.js');
+
+// GET /quizes/:quizId/comments/new
+exports.new=function(req,res){
+  res.render('comments/new.ejs',{quizid: req.params.quizId, errors: []});
+};
+
+// POST /quizes/:quizId/comments
+exports.create=function(req,res) {
+  var comment=models.Comment.build(
+    { texto: req.body.comment.texto,
+    QuizId: req.params.quizId
+  });
+  var errors = comment.validate();
+  if (errors) {
+    var i=0; var errores=new Array();
+    for (var prop in errors) errores[i++]={message:errors[prop]};
+    res.render('comments/new.ejs',
+      {comment: comment, quizid:req.params.quizId, errors:errors.errors});
+  } else {
+    comment
+    .save({fields:["texto","QuizId"]})
+    .then(function() {res.redirect('/quizes/'+req.params.quizId)})
+    //.catch(function(error){next(error)});
+  }
+};
